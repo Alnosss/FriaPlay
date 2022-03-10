dad35474a6dd85c901655bf198a3855d6131ce93
Alexander Carling <alca2012@edu.laroverken.se>
2022-03-10 14:01:38 +0100

    a

---
+++ FriaPlay/src/assets/Player-test.js
@@ -0,0 +1,8 @@
+const disc = document.querySelector('.disc');
+
+function playBtn(){
+ /*   playBtn.addEventListener('click', () => {
+        disk.classList.toggle('play');
+    }) */
+    alert("Ja");
+}
